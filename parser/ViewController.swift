616b16cf21523ec16e53e1452175d080783de1f5
Tammy Bearly <tbearly@colostate.edu>
2020-02-13 15:49:36 -0700

    Initial Commit

---
+++ parser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  parser
+//
+//  Created by Brittney Bearly on 2/13/20.
+//  Copyright © 2020 Tammy Bearly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
